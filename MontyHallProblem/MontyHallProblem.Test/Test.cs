c66e14f5c2691170b2a827d6b828ae4640b7de19
selma.ardelean <selma.ardelean@newvoicemedia.com>
2015-06-29 18:18:02 +0100

    added test project and test class stub

---
+++ MontyHallProblem/MontyHallProblem.Test/Test.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace MontyHallProblem.Test
+{
+    [TestFixture]
+    public class Test
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            
+        }
+
+        [Test]
+        public void Test1()
+        {
+            
+        }
+    }
+}
